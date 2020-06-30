d9fe8e120a6938e6ef39886139aac59ee295b44e
markfirmware <markfirmware.users.noreply.github.com>
2020-06-29 23:14:00 -0400

    .

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { time } from './stores.js';
+	const formatter = new Intl.DateTimeFormat('en', {
+		hour12: true,
+		hour: 'numeric',
+		minute: '2-digit',
+		second: '2-digit'
+	});
+</script>
+
+<h1>The time is {formatter.format($time)}</h1>
+<h2>
+	<ul>
+		<li>A</li>
+		<li>B</li>
+	</ul>
+</h2>
