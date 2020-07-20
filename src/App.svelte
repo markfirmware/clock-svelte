9927d4f4d90ab5065cdbca0f2e6a4ce35b3b7cc4
markfirmware <mark.kendrat@gmail.com>
2020-07-20 05:11:54 +0000

    .

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 </svelte:head>
 
 <script>
+	import { slide } from 'svelte/transition';
 	import { onMount } from 'svelte';
 
 	let civil_twilight_begin = new Date("2020-07-19T10:02:09+00:00");
@@ -13,18 +14,19 @@
 
 	let now = new Date();
 	
-	$: hide = ms > 800;
+	$: hide = seconds == 59 && ms > 500;
 	$: d = now.getDate();
 	$: hours = now.getHours();
 	$: minutes = now.getMinutes();
 	$: seconds = now.getSeconds();
 	$: ms = now.getMilliseconds();
 	$: time = 
-		'Correct Time: ' + (hours % 12 == 0 ? 12 : hours % 12) + ':' + (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
-	$: day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][now.getDay()] + ' ' + (hours < 6 ? 'Night' : hours < 12 ? 'Morning' : hours < 18 ? 'Afternoon' : hours < 21 ? 'Evening' : 'Night');
+		'Correct Time: ' + (hours % 12 == 0 ? 12 : hours % 12) + ':' + (minutes < 10 ? '0' : '') + minutes;
+	$: day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][now.getDay()] + ' ' + (hours < 12 ? 'Morning' : hours < 18 ? 'Afternoon' : hours < 21 ? 'Evening' : 'Night');
 	$: date = ['January','February','March','April','May','June','July','August','September','October','November','December'][now.getMonth()] + ' ' + d + (d == 1 || d == 21 ||  d == 31 ? "st" : d == 2 || d == 22 ? "nd" : d == 3 || d == 23 ? "rd" : "th") + ' ' + now.getFullYear();
     $: until_light = light_begin - (hours * 60 + minutes);
     $: until_dark = light_end - (hours * 60 + minutes);
+    $: until = near(until_dark, 'dark') + near(until_light, 'light');
 
 	onMount(() => {
 		const interval = setInterval(() => {
@@ -35,12 +37,16 @@
 			clearInterval(interval);
 		};
 	});
+
+    function near(minutes, name) {
+        return minutes < 1 || minutes > 59 ? '' : '(' + minutes + ' minute' + (minutes == 1 ? '' : 's') + ' until ' + name + ')';
+    }
 </script>
 
 <style>
 	.hide {
-			opacity:0;
-			transition:opacity 0.3s
+		opacity:0;
+		transition:opacity 0.7s
 	}
 	.page {
 		background-color:black;
@@ -53,12 +59,17 @@
 
 <div class=page>
   <div class:hide>
-		{time}
+	{time}
   </div>
   <div>	
-		{day} {#if until_light >= 1 && until_light <= 59}{until_light} minute{#if until_light != 1}s{/if} until light{/if}{#if until_dark >= 1 && until_dark <= 59}{until_dark} minute{#if until_light != 1}s{/if} until dark{/if}
+	{day}
   </div>
+  {#if until != ''}
+    <div transition:slide={{duration:800}}>
+      {until}
+    </div>
+  {/if}
   <div>	
-		{date}
+	{date}
   </div>
 </div>
