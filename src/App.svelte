7588ee7c1ab0b3ab394574568af1b07c3ff08781
markfirmware <markfirmware@users.noreply.github.com>
2020-07-24 19:48:51 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,8 +6,8 @@
 	import { slide } from 'svelte/transition';
 	import { onMount } from 'svelte';
 
-	let civil_twilight_begin = new Date("2020-07-19T10:02:09+00:00");
-	let civil_twilight_end = new Date("2020-07-20T01:39:55+00:00");
+	let civil_twilight_begin = new Date("2020-07-24T10:06:51+00:00");
+	let civil_twilight_end = new Date("2020-07-25T01:35:30+00:00");
 
 	let light_begin = civil_twilight_begin.getHours() * 60 + civil_twilight_begin.getMinutes();
 	let light_end = civil_twilight_end.getHours() * 60 + civil_twilight_end.getMinutes();
@@ -24,9 +24,9 @@
 		'Correct Time: ' + (hours % 12 == 0 ? 12 : hours % 12) + ':' + (minutes < 10 ? '0' : '') + minutes;
 	$: day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'][now.getDay()] + ' ' + (hours < 12 ? 'Morning' : hours < 18 ? 'Afternoon' : hours < 21 ? 'Evening' : 'Night');
 	$: date = ['January','February','March','April','May','June','July','August','September','October','November','December'][now.getMonth()] + ' ' + d + (d == 1 || d == 21 ||  d == 31 ? "st" : d == 2 || d == 22 ? "nd" : d == 3 || d == 23 ? "rd" : "th") + ' ' + now.getFullYear();
-    $: until_light = light_begin - (hours * 60 + minutes);
-    $: until_dark = light_end - (hours * 60 + minutes);
-    $: until = near(until_dark, 'dark') + near(until_light, 'light');
+	$: until_light = light_begin - (hours * 60 + minutes);
+	$: until_dark = light_end - (hours * 60 + minutes);
+	$: until = near(until_dark, 'dark') + near(until_light, 'light');
 
 	onMount(() => {
 		const interval = setInterval(() => {
